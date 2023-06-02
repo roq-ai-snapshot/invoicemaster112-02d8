729a96c43d91ef198cade6d01b13ee7533fbcb5f
roq-ai <savin171099@gmail.com>
2023-06-02 14:26:21 +0000

    Initial commit

---
+++ src/interfaces/organisation/index.ts
@@ -0,0 +1,21 @@
+import { ContractInterface } from 'interfaces/contract';
+import { InvoiceInterface } from 'interfaces/invoice';
+import { OrganisationUserInterface } from 'interfaces/organisation-user';
+import { UserInterface } from 'interfaces/user';
+
+export interface OrganisationInterface {
+  id?: string;
+  name: string;
+  user_id: string;
+  created_at?: Date;
+  updated_at?: Date;
+  contract?: ContractInterface[];
+  invoice?: InvoiceInterface[];
+  organisation_user?: OrganisationUserInterface[];
+  user?: UserInterface;
+  _count?: {
+    contract?: number;
+    invoice?: number;
+    organisation_user?: number;
+  };
+}
